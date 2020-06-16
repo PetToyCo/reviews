f0c43fec4ce376be4b9093668f14fc979c25836c
Nick-Zabalza <nickyz2565@gmail.com>
2020-06-16 15:27:03 -0700

    Added linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-  mode: "production",
-  entry: path.resolve(__dirname, "client", "src", "service.jsx"),
+  mode: 'production',
+  entry: path.resolve(__dirname, 'client', 'src', 'service.jsx'),
   output: {
-    path: path.resolve(__dirname, "client", "public"),
-    filename: "app.js"
+    path: path.resolve(__dirname, 'client', 'public'),
+    filename: 'app.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        loader: babel-loader,
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-react", "@babel/preset-env"]
-        }
+          presets: ['@babel/preset-react', '@babel/preset-env'],
+        },
       },
       {
         test: /\.js?$/,
-        loader: babel-loader,
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env"]
-        }
-      }
-    ]
-  }
-}
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
+  },
+};
