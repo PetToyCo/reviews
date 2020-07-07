7ee2a687aeabb093ef46f3aa3cb5eae8fbac70ef
Nick-Zabalza <nickyz2565@gmail.com>
2020-07-07 15:44:12 -0700

    Reverting code to be used with proxy before merge

--- client/src/service.jsx
+++ client/src/service.jsx
@@ -24,33 +24,7 @@ class ReviewsModule extends React.Component {
     //call just below. Make sure to switch back just before pushing up to repo.
     //Just make sure to run webpack again so bundle is correct (In repo's cd, run >npm run build)
     //start of service as standalone
-    axios.get('http://127.0.0.1:3001/reviews/100')
-      .then((results) => {
-        const { reviewAverage, numberOfReviews, allReviews } = results.data;
-
-        dispatchUpdateReviewAverage(reviewAverage);
-        dispatchUpdateNumberOfReviews(numberOfReviews);
-        dispatchUpdateAllReviews(allReviews);
-        dispatchUpdateFilteredReviews(allReviews);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-    //end of service as standalone
-
-    //start of service as proxy service
-    // const { search } = window.location;
-    // const searchSplit = search.split('&');
-    // let splitItemID;
-
-    // for (let i = 0; i < searchSplit.length; i++) {
-    //   if (searchSplit[i].includes('itemID')) {
-    //     splitItemID = searchSplit[i].split('=');
-    //     break;
-    //   }
-    // }
-
-    // axios.get(`http://127.0.0.1:3001/reviews/${splitItemID[1]}`)
+    // axios.get('http://127.0.0.1:3001/reviews/100')
     //   .then((results) => {
     //     const { reviewAverage, numberOfReviews, allReviews } = results.data;
 
@@ -58,16 +32,42 @@ class ReviewsModule extends React.Component {
     //     dispatchUpdateNumberOfReviews(numberOfReviews);
     //     dispatchUpdateAllReviews(allReviews);
     //     dispatchUpdateFilteredReviews(allReviews);
-
-    //     if (numberOfReviews === 0) {
-    //       store.dispatch(updateReviewRange('RESET', [0, -1]));
-    //     } else if (numberOfReviews < 8) {
-    //       store.dispatch(updateReviewRange('RESET', [0, numberOfReviews - 1]));
-    //     }
     //   })
     //   .catch((err) => {
     //     console.log(err);
     //   });
+    //end of service as standalone
+
+    //start of service as proxy service
+    const { search } = window.location;
+    const searchSplit = search.split('&');
+    let splitItemID;
+
+    for (let i = 0; i < searchSplit.length; i++) {
+      if (searchSplit[i].includes('itemID')) {
+        splitItemID = searchSplit[i].split('=');
+        break;
+      }
+    }
+
+    axios.get(`http://127.0.0.1:3001/reviews/${splitItemID[1]}`)
+      .then((results) => {
+        const { reviewAverage, numberOfReviews, allReviews } = results.data;
+
+        dispatchUpdateReviewAverage(reviewAverage);
+        dispatchUpdateNumberOfReviews(numberOfReviews);
+        dispatchUpdateAllReviews(allReviews);
+        dispatchUpdateFilteredReviews(allReviews);
+
+        if (numberOfReviews === 0) {
+          store.dispatch(updateReviewRange('RESET', [0, -1]));
+        } else if (numberOfReviews < 8) {
+          store.dispatch(updateReviewRange('RESET', [0, numberOfReviews - 1]));
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     //end of service as proxy service
   }
 
@@ -76,7 +76,7 @@ class ReviewsModule extends React.Component {
     return (
       <div id="reviews-module" style={{ display: 'flex', flexDirection: 'column', flexShrink: '0' }}>
         {/*The tag directly below this is for testing purposes only. Make sure to comment out before building for production*/}
-        <div id='MODAL_ATTACH_POINT' />
+        {/* <div id='MODAL_ATTACH_POINT' /> */}
         <ReviewHeader />
         <ReviewBody />
         <Reviews />
