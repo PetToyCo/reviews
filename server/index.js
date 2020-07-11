9025b7f64dfe77a85e6b80b2ec3f0630eb97e701
Nick-Zabalza <nickyz2565@gmail.com>
2020-07-11 10:27:49 -0700

    WIP

--- server/index.js
+++ server/index.js
@@ -8,12 +8,15 @@ const server = express();
 server.use(morgan('dev'));
 server.use(function(req, res, next) {
   const { referer } = req.headers;
-  if (referer.includes('http://127.0.0.1:3000')) {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
-  } else if (referer.includes('http://127.0.0.1:3005')) {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3005');
-  } else if (referer.includes('http://127.0.0.1:3004')) {
-    res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3004');
+
+  if (referer) {
+    if (referer.includes('http://127.0.0.1:3000')) {
+      res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
+    } else if (referer.includes('http://127.0.0.1:3005')) {
+      res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3005');
+    } else if (referer.includes('http://127.0.0.1:3004')) {
+      res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:3004');
+    }
   }
   next();
 });
